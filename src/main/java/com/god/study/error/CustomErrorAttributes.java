b34f803fd3aae78396036ed95e036cac65360e8f
dedurox2@gmail.com <dedurox2@gmail.com>
2020-11-17 17:58:21 +0900

    [20201117]#1.Test Controller Setting

---
+++ src/main/java/com/god/study/error/CustomErrorAttributes.java
@@ -0,0 +1,18 @@
+package com.god.study.error;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Component
+public class CustomErrorAttributes extends DefaultErrorAttributes {
+
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        Map<String, Object> result = super.getErrorAttributes(webRequest, includeStackTrace);
+        result.put("greeting", "Hello");
+        return result;
+    }
+}
