14e497c2adfd79a749655ceaf7d532decf1c50e7
dedurox2@gmail.com <dedurox2@gmail.com>
2020-12-04 18:04:10 +0900

    [20201204]#1.Error Page Setting

--- src/main/java/com/god/study/error/controller/CustomErrorController.java
+++ src/main/java/com/god/study/error/controller/CustomErrorController.java
@@ -1,20 +1,34 @@
 package com.god.study.error.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@Slf4j
 public class CustomErrorController implements ErrorController {
     private String VIEW_PATH = "thymeleaf/error/";
 
-    @RequestMapping(value = "/error")
-    public String handleError(HttpServletRequest request) {
+    /*@RequestMapping(value = "/error")
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object status2 = request.getAttribute("javax.servlet.error.status_code");
+
+        Object exceptionObj = request.getAttribute("javax.servlet.error.exception");
+        Object exceptionObj2 = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        if(exceptionObj != null){
+            Throwable e = ((Exception) exceptionObj2).getCause();
+            model.addAttribute("exception",e.getClass().getName());
+            model.addAttribute("message",e.getMessage());
+        }
 
         if(status != null){
             int statusCode = Integer.valueOf(status.toString());
@@ -27,10 +41,46 @@ public class CustomErrorController implements ErrorController {
             }
         }
         return "error";
+    }*/
+
+    @RequestMapping("/error")
+    public ModelAndView handleError(HttpServletRequest request, HttpServletResponse response) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        ModelAndView modelAndView = new ModelAndView();
+        System.out.println(response.getStatus());
+
+        Object exceptionObj = request.getAttribute("javax.servlet.error.exception");
+        Object exceptionObj2 = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        if(exceptionObj2 != null){
+            Throwable e = ((Exception) exceptionObj2).getCause();
+            //model.addAttribute("exception",e.getClass().getName());
+            //model.addAttribute("message",e.getMessage());
+
+            System.out.println(e.getClass().getName());
+            System.out.println(e.getMessage());
+        }
+
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                modelAndView.setViewName(VIEW_PATH + "404");
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                modelAndView.setViewName("/errorPage/500");
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                modelAndView.setViewName("/errorPage/403");
+            } else modelAndView.setViewName("/errorPage/common");
+        }
+        return modelAndView;
     }
 
+
+
     @Override
+    /* 스프링 부트 2.3.x부터 다음 메서드는 deprecated 되었음.
+    *  custom path를 지정하려면 server.error.path 속성으로 지정 가능.
+    *  */
     public String getErrorPath() {
-        return "/error";
+        return null;
     }
 }
